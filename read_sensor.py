262f49cc3f221cbf529bbb4c9464dc7e56a10c38
Pi at bklebel <bklebel@github>
2020-02-08 14:54:56 +0100

    introducing noisefunction

--- read_sensor.py
+++ read_sensor.py
@@ -8,17 +8,37 @@ import time
 from functions import get_data
 from functions import create_logger
 
+import numpy as np
+
+from prometheus_client import start_http_server
+from prometheus_client import Gauge
 
 # setup db connection
 # con = connect_db()
 # cursor = con.cursor()
 
 # set sleep duration for test purposes
-sleep_duration = 30
+sleep_duration = 60
 
 # set up logging for debugging
 logger_events = create_logger('read_sensor.log', 'events')
 logger_data = create_logger(file='data.log', log='temps', form='%(asctime)s %(message)s')
+
+
+def eliminateNoise(values, std_factor = 2):
+    mean = np.mean(values)
+    standard_deviation = np.std(values)
+
+    if standard_deviation == 0:
+        return np.mean(values)
+
+    final_values = np.nan_like(values)
+    final_values = values[np.where(values > mean - std_factor * standard_deviation)]
+    final_values[np.where(np.isnan(final_values))] = values[np.where(values < mean + std_factor * standard_deviation)]
+    final_values = [element for element in values if element > mean - std_factor * standard_deviation]
+    final_values = [element for element in final_values if element < mean + std_factor * standard_deviation]
+
+    return np.mean(final_values)
 
 
 # function for writing results to database
@@ -43,10 +63,15 @@ logger_data = create_logger(file='data.log', log='temps', form='%(asctime)s %(me
 
 if __name__ == '__main__':
     logger_events.info(f'Reading data and writing to database every {sleep_duration} seconds.')
+    prom_temp = Gauge('Temperature', 'Temperature measured by the DHT22 Sensor')
+    prom_humid = Gauge('Humidity', 'Relative Humidity measured by the DHT22 Sensor')
+    start_http_server(8000)
     while True:
         date, humidity, temperature = get_data()
         print(date, humidity, temperature)
-        logger_data.info(f'; {humidity}; {temperature}')
+        # logger_data.info(f'; {humidity}; {temperature}')
+        prom_temp.set(temperature)
+        prom_humid.set(humidity)
 
         # write_to_db(cursor, time, humidity, temperature)
 
