e6332caf7873b52c9c6166863ac19a674c96d944
Pi at bklebel <bklebel@github>
2020-02-09 18:46:59 +0100

    add 1s sleep between 10 measurements, for decreased noise probability

--- read_sensor.py
+++ read_sensor.py
@@ -42,19 +42,20 @@ class MyDaemon(Daemon):
         main()
 
 if __name__ == '__main__':
-    daemon = MyDaemon('/tmp/daemon-telegram-bot.pid')
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print("Unknown command")
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print("usage: {} start|stop|restart".format(
-            sys.argv[0]))  # % sys.argv[0]
-        sys.exit(2)
+    main()
+    # daemon = MyDaemon('/tmp/daemon-telegram-bot.pid')
+    # if len(sys.argv) == 2:
+    #     if 'start' == sys.argv[1]:
+    #         daemon.start()
+    #     elif 'stop' == sys.argv[1]:
+    #         daemon.stop()
+    #     elif 'restart' == sys.argv[1]:
+    #         daemon.restart()
+    #     else:
+    #         print("Unknown command")
+    #         sys.exit(2)
+    #     sys.exit(0)
+    # else:
+    #     print("usage: {} start|stop|restart".format(
+    #         sys.argv[0]))  # % sys.argv[0]
+    #     sys.exit(2)
